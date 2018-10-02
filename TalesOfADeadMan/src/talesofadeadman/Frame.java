986e88ebb825274d506cf59de57d4f98d5935e6e
Kevin Sutton <kssutton@alaska.edu>
2018-10-01 16:23:05 -0800

    test Pixels
    Added a test pixel to the Frame to test drawing pixels

--- TalesOfADeadMan/src/talesofadeadman/Frame.java
+++ TalesOfADeadMan/src/talesofadeadman/Frame.java
@@ -13,6 +13,8 @@
 
 package talesofadeadman;
 
+import java.awt.Graphics;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 
 /**
@@ -20,6 +22,8 @@ import javax.swing.JFrame;
  * @author Kevin
  */
 public class Frame extends JFrame{
+    
+    Pixel test;
     
     //////////////////////////////////////////////////////////////////////////////
     //  Descritpion: This function serves as the constructor for the Frame
@@ -35,6 +39,12 @@ public class Frame extends JFrame{
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(800, 800);
+        
+        test = new Pixel("blackPixel.jpg");
+        
+        this.setLayout(new GridLayout(50,50));
+        this.add(test);
+        test.setVisible(true);
     }
 
 }
